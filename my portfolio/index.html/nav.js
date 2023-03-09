2a02bc3b2da0cb5f09145accb635427731726c20
Fasna c <87683296+fasnamfd@users.noreply.github.com>
2023-03-09 18:15:58 +0530

    Add files via upload

---
+++ my portfolio/index.html/nav.js
@@ -0,0 +1,17 @@
+// Get the navigation bar element
+var navbar = document.getElementById("my-navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the "sticky" class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// Add the "myFunction" function to the window object's "scroll" event
+window.onscroll = function() {myFunction()};
